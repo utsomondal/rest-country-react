eed38c15c9faa432b7dbbb61c48513c4d069f785
utsomondal <utsomondal2019@gmail.com>
2024-03-11 09:27:04 +0600

    data rendering

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import './country.css'
 const Country = ({ country }) => {
-    const { name, flags } = country;
+    const { name, flags,population,capital } = country;
     return (
         <div className="country">
             <div>
@@ -8,6 +8,8 @@ const Country = ({ country }) => {
             </div>
             <div className='details-container'>
                 <h3>Name: {name?.common}</h3>
+                <h3>Population: {population}</h3>
+                <h3>Capital: {capital}</h3>
             </div>
         </div>
     );
